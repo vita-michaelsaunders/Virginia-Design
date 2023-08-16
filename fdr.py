3ab9fd7e6659ce50fb5d465a2a1b93937e3e3aca
vita-michaelsaunders <michael.saunders@vita.virginia.gov>
2023-08-16 16:00:03 -0400

    Setting update for FDR actions

---
+++ fdr.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copyfile('./medias/Categorised/developer/Virginia Design/assets/css/styles_dev.css','medias/Categorised/developer/Virginia Design/assets/css/styles.css')
+shutil.copyfile('./medias/Categorised/developer/Virginia Design/assets/css/styles_dev.css.map','medias/Categorised/developer/Virginia Design/assets/css/styles.css.map')
